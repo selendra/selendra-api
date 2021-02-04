02e6d39afe0252bf5a56d34b22719071a6e01b36
combo <combo@combos-MacBook-Pro.local>
2021-02-04 13:27:08 +0700

    init

--- src/index.js
+++ src/index.js
@@ -6,17 +6,21 @@ import BigNumber from "bignumber.js";
 const CreateAccount = async ({username, type}) => {
   const keyring = new Keyring({ type: 'sr25519', ss58Format: 2 });
   const mnemonic = mnemonicGenerate(12);
+
   if(!username) throw new Error ('username could not be null');
   if(!type) throw new Error ('type could not be null');
+
   const pair = keyring.addFromUri(mnemonic, { name: username }, type);
   return { mnemonic, pair };
 }
 
 const ImportAccount = async ({seed, type}) => {
   const keyring = new Keyring({ ss58Format: 2 });
+
   if(mnemonicValidate(seed) === false) throw new Error ('Seed is not valid!');
   if(!seed) throw new Error ('seed could not be null');
   if(!type) throw new Error ('type could not be null');
+  
   const pair = keyring.addFromUri(seed, {} ,type);
   return { pair };
 }
